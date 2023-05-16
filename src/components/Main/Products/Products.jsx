c72510a9f752bd6fed5321f93228012b44363faa
Maxim <mmandrrr@gmail.com>
2023-05-16 11:44:13 +0300

    added section products and adaptive for it

--- src/components/Main/Products/Products.jsx
+++ src/components/Main/Products/Products.jsx
@@ -1,20 +1,28 @@
+import ProductItem from './ProductItem/ProductItem';
 
+import { productData } from './productsData';
 
 const Products = () => {
+
+    const productList = productData.map(({id,title,img,description},i) => {
+        return(
+            <ProductItem 
+                id={id}
+                title={title}
+                img={img}
+                description={description}
+                key={i}
+            />
+        )
+    })
+
     return(
         <section className="products">
             <div className="products__container container">
                 <h3 className="products__subtitle subtitle">100% natural</h3>
                 <h2 className="products__title title">Products</h2>
                 <div className="products__wrapper">
-                    <div className="products__item">
-                        <img src="" alt="Product Image" className="products__img" />
-                        <h4 className="products__item-title">ice cream</h4>
-                        <p className="products__description fz-16_reg">
-                            Soft ice cream with a delicate taste. Made with milk, cream, sugar and natural stabilizer
-                        </p>
-                        <img src="" alt="arrow" className="products__arrow" />
-                    </div>
+                    {productList}
                 </div>
             </div>
         </section>
---
+++ src/components/Main/Products/productsData.js
@@ -0,0 +1,24 @@
+import iceCream from '../../../assets/products/ice-cream-min.png';
+import iceCoffee from '../../../assets/products/ice-coffee-min.png';
+import milkshake from '../../../assets/products/milkshake-min.png';
+
+export const productData = [
+    {
+        id: 'ice-cream',
+        img: iceCream,
+        title: 'ice cream',
+        description: 'Soft ice cream with a delicate taste. Made with milk, cream, sugar and natural stabilizer',
+    },
+    {
+        id: 'ice-coffee',
+        img: iceCoffee,
+        title: 'ice coffee',
+        description: 'Fragrant invigorating drink created from 100% natural ingredients without the use of preservatives and flavor enhancers',
+    },
+    {
+        id: 'milkshake',
+        img: milkshake,
+        title: 'milkshakes',
+        description: 'Sweet drinks based on milk and ice cream, supplemented with syrups, fruits, berries and other sweets of your choice',
+    }
+]
